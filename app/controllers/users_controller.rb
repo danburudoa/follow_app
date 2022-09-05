37bc6bab9352cb2e0ef811c966d726f35ee295d3
danburudoa <yumamukai515@gmail.com>
2022-09-05 11:40:44 +0900

    user実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  # あるユーザーがフォローしている人全員を取得するアクション
+  def followings
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  # あるユーザーをフォローしている人全員を取得するアクション
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
   end
 end
